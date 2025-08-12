949d57de58cc73eb93392bdbccd83b506a520939
Veronikanababan14 <veronikanbbn14@gmail.com>
2025-08-12 14:01:02 +0700

    Memperbaharui navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -32,7 +32,7 @@
               <a href="#" class="block py-2 px-3 text-orange-500 bg-blue-700 rounded-sm md:bg-transparent md:p-0 font-bold" aria-current="page">Login</a>
             </li>
             <li>
-              <a href="#" class="flex justify-center items-center text-white rounded-sm bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 px-4 py-2 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-bold">
+              <a href="#" class="flex justify-center items-center text-white rounded-sm bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 px-4 py-3 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-bold">
   Sign Up
 </a>
 
